3ad149feb730b0f5af1971049851ebab39605145
Adam <adam.e.sheaffer@gmail.com>
2020-02-28 09:31:06 -0600

    while loop added

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace GuessingGame
             Random rand = new Random();
             int secretNumber = rand.Next(1, 11);
 
-            for (int i = 0; i < 3; i++)
+            while (true)
             {
                 Console.WriteLine("Guess a number between 1 and 10");
                 string userGuess = Console.ReadLine();
@@ -18,7 +18,7 @@ namespace GuessingGame
                 if (userGuessInt == secretNumber)
                 {
                     Console.WriteLine($"You guessed it! It was {secretNumber}");
-                    return;
+                    break;
                 }
                 else
                 {
